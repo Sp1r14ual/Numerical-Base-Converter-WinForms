32a3d59095412bbe9bb0e328c61a1a2bccdb73e0
sp1r14ual <sp1r14ual@gmail.com>
2024-02-17 16:43:10 +0700

    Add project files.

---
+++ Numerical-Base-Converter-WinForms/Form1.cs
@@ -0,0 +1,10 @@
+namespace Numerical_Base_Converter_WinForms
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+    }
+}
